9b283449e339e5a5c62b5cb4dcaee377be60e505
almero0922 <albertomedel29@gmail.com>
2023-09-10 19:24:07 -0500

    Challenge Done

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ const buttonForm = document.getElementById("buttonForm");
 const customBform = document.getElementById("custom%");
 const peopleInput = document.getElementById("peopleInput");
 const peopleForm = document.getElementById("peopleForm");
+const resetButton = document.getElementById("reset-ca");
 var buttonClicked = "";
 var customValue = 0;
 var validNumber = new RegExp(/^\d*\.?\d*$/);
 var validNum = new RegExp(/^\d+$/);
 var bill = 0;
 var people = 0;
+
+billInput.value = "";
+customBform.value = "";
+peopleInput.value = "";
 
 billForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -53,13 +58,33 @@ peopleForm.addEventListener("submit", (e) => {
   setTotalAmount();
 });
 
-function setTotalAmount(){
+function setTotalAmount() {
   let bill = parseFloat(billInput.value);
   let people = parseInt(peopleInput.value);
-  let percentage = customValue ? parseFloat(customValue) : parseFloat(buttonClicked);
+  let percentage = customValue
+    ? parseFloat(customValue)
+    : parseFloat(buttonClicked);
   let totalP = (bill / people) * percentage;
-  let total = totalP + (bill / people);
+  let total = totalP + bill / people;
 
-  document.getElementById('result-0').innerText = totalP;
-  document.getElementById('result-1').innerText = total;
+
+  document.getElementById("result-0").innerText = totalP.toFixed(2);
+  document.getElementById("result-1").innerText = total.toFixed(2);
 }
+
+resetButton.addEventListener("click", (e) => {
+  buttonClicked = "";
+  bill = 0;
+  people = 0;
+  customValue = 0;
+  buttonMatrix.forEach((button) => {
+    if (button.classList.contains("button-clicked")) {
+      button.classList.remove("button-clicked");
+    }
+  });
+  document.getElementById("result-0").innerText = "$0.00";
+  document.getElementById("result-1").innerText = "$0.00";
+  billInput.value = "";
+  customBform.value = "";
+  peopleInput.value = "";
+});
